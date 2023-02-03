2ef5dc250cc66e5f91cad1e209b9fead40e3b1a2
pallavi <pallavichaudhari92@gmail.com>
2023-02-03 09:33:47 +0100

    config file updated

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-DATA = "G:/Python/workplace/diabetes/CSV_FILE/diabetes.csv"
+DATA = r"G:/Python/workplace/diabetes/CSV_FILE/diabetes.csv"
 
-MODEL =r"G:/Python/diabetes/Artifacts/kngs.pkl"
+MODEL =r"G:/Python/workplace/diabetes/Artifacts/kngs.pkl"
 
 PORT  = 5555
